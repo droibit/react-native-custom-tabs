42c6cb76b249fa919ec2ee5097bdde718b24464c
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-25 08:33:59 +0900

    Commit course the way

---
+++ android/src/main/java/com/github/droibit/android/reactnative/customtabs/ChromePackage.java
@@ -0,0 +1,20 @@
+package com.github.droibit.android.reactnative.customtabs;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kumagai on 16/04/25.
+ */
+/* package */ class ChromePackage {
+
+    /* package */ static final String PACKAGE_STABLE = "com.android.chrome";
+    /* package */ static final String PACKAGE_BETA = "com.chrome.beta";
+    /* package */ static final String PACKAGE_DEV = "com.chrome.dev";
+    /* package */ static final String PACKAGE_LOCAL = "com.google.android.apps.chrome";
+    /* package */ static final List<String> CHROME_PACKAGES = Arrays.asList(
+            PACKAGE_STABLE,
+            PACKAGE_BETA,
+            PACKAGE_DEV,
+            PACKAGE_LOCAL);
+}
--- android/src/main/java/com/github/droibit/android/reactnative/customtabs/CustomTabsModule.java
+++ android/src/main/java/com/github/droibit/android/reactnative/customtabs/CustomTabsModule.java
@@ -1,6 +1,10 @@
 package com.github.droibit.android.reactnative.customtabs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
@@ -13,10 +17,16 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.common.MapBuilder;
+import com.facebook.react.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nullable;
+
+import static android.content.pm.PackageManager.*;
+import static com.github.droibit.android.reactnative.customtabs.ChromePackage.*;
 
 /**
  * CustomTabs module.
@@ -24,29 +34,27 @@ import javax.annotation.Nullable;
 public class CustomTabsModule extends ReactContextBaseJavaModule {
 
     private static final String KEY_TOOLBAR_COLOR = "toolbarColor";
-    private static final String KEY_ENABLE_URLBAR_HIDING = "enableUrlBarHiding";
-    //private static final String KEY_CLOSE_BUTTON_ICON = "closeButtonIcon"; // TODO
+    private static final String KEY_ENABLE_URL_BAR_HIDING = "enableUrlBarHiding";
     private static final String KEY_SHOW_PAGE_TITLE = "showPageTitle";
     private static final String KEY_DEFAULT_SHARE_MENU_ITEM = "enableDefaultShare";
     private static final String KEY_ANIMATIONS = "animations";
-
-    private static final String KEY_ANIMATIONS_SLIDE = "animationsSlide";
-    private static final String KEY_ANIMATIONS_FADE = "animationsFade";
+    private static final String KEY_PRIORITY = "priority";
 
     private static final int ANIMATIONS_SLIDE = 0;
     private static final int ANIMATIONS_FADE = 1;
+
+    private static final int PRIORITY_DEFAULT = 0;
+    private static final int PRIORITY_HIGH = 1;
 
     private static final Map<String, Object> CONSTANTS;
     static {
         CONSTANTS = MapBuilder.newHashMap();
         CONSTANTS.put(KEY_TOOLBAR_COLOR, KEY_TOOLBAR_COLOR);
-        CONSTANTS.put(KEY_ENABLE_URLBAR_HIDING, KEY_ENABLE_URLBAR_HIDING);
-        //KEY_CLOSE_BUTTON_ICON, KEY_CLOSE_BUTTON_ICON,
+        CONSTANTS.put(KEY_ENABLE_URL_BAR_HIDING, KEY_ENABLE_URL_BAR_HIDING);
         CONSTANTS.put(KEY_SHOW_PAGE_TITLE, KEY_SHOW_PAGE_TITLE);
         CONSTANTS.put(KEY_DEFAULT_SHARE_MENU_ITEM, KEY_DEFAULT_SHARE_MENU_ITEM);
+        CONSTANTS.put(KEY_PRIORITY, KEY_PRIORITY);
         CONSTANTS.put(KEY_ANIMATIONS, KEY_ANIMATIONS);
-        CONSTANTS.put(KEY_ANIMATIONS_SLIDE, ANIMATIONS_SLIDE);
-        CONSTANTS.put(KEY_ANIMATIONS_FADE, ANIMATIONS_FADE);
     }
 
     private static final String MODULE_NAME = "CustomTabsManager";
@@ -75,7 +83,7 @@ public class CustomTabsModule extends ReactContextBaseJavaModule {
      * @param url the URL to open
      */
     @ReactMethod
-    public void openURL(String url, ReadableMap options, Promise promise) {
+    public void openURL(String url, ReadableMap option, Promise promise) {
         if (TextUtils.isEmpty(url)) {
             promise.reject(new JSApplicationIllegalArgumentException("Invalid URL: " + url));
             return;
@@ -87,9 +95,69 @@ public class CustomTabsModule extends ReactContextBaseJavaModule {
             return;
         }
 
+        if (!canOpen(uri)) {
+            promise.resolve(false);
+            return;
+        }
+
+        final CustomTabsIntent customTabsIntent = buildIntent(option, promise);
+        try {
+            if (getCurrentActivity() != null) {
+                customTabsIntent.launchUrl(getCurrentActivity(), uri);
+                promise.resolve(true);
+            }
+            // FIXME: activity == null
+        } catch (Exception e) {
+            promise.reject(new JSApplicationIllegalArgumentException(
+                    "Could not open URL '" + url + "': " + e.getMessage()));
+        }
+    }
+
+    @VisibleForTesting
+    /* package */ boolean canOpen(Uri uri) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        final PackageManager pm = getReactApplicationContext().getPackageManager();
+        final List<ResolveInfo> resolvedActivities = pm.queryIntentActivities(intent, 0);
+        return !resolvedActivities.isEmpty();
+    }
+
+    @Nullable
+    @VisibleForTesting
+    /* package */ String packageNameToUse(boolean ignoreDefault) {
+        final PackageManager pm = getReactApplicationContext().getPackageManager();
+        final List<ApplicationInfo> installedApps = pm.getInstalledApplications(GET_META_DATA);
+
+        final List<String> installedChromes = new ArrayList<>(CHROME_PACKAGES.size());
+        for (ApplicationInfo app : installedApps) {
+            if (CHROME_PACKAGES.contains(app.packageName)) {
+                installedChromes.add(app.packageName);
+            }
+        }
+
+        if (installedChromes.isEmpty()) {
+            return null;
+        }
+
+        // Stable comes first.
+        for (String chromePackage : CHROME_PACKAGES) {
+            if (installedChromes.contains(chromePackage)) {
+                return chromePackage;
+            }
+        }
+        return null;
+    }
+
+    @VisibleForTesting
+    /* package */ CustomTabsIntent buildIntent(ReadableMap option, Promise promise) {
         final CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        if (options.hasKey(KEY_TOOLBAR_COLOR)) {
-            final String colorString = options.getString(KEY_TOOLBAR_COLOR);
+
+        final int priority = option.hasKey(KEY_PRIORITY)
+                ? option.getInt(KEY_PRIORITY) : PRIORITY_DEFAULT;
+
+        // TODO:
+
+        if (option.hasKey(KEY_TOOLBAR_COLOR)) {
+            final String colorString = option.getString(KEY_TOOLBAR_COLOR);
             try {
                 builder.setToolbarColor(Color.parseColor(colorString));
             } catch (IllegalArgumentException e) {
@@ -97,20 +165,20 @@ public class CustomTabsModule extends ReactContextBaseJavaModule {
                         "Invalid toolbar color '" + colorString + "': " + e.getMessage()));
             }
         }
-        if (options.hasKey(KEY_ENABLE_URLBAR_HIDING) ||
-                options.getBoolean(KEY_ENABLE_URLBAR_HIDING)) {
+        if (option.hasKey(KEY_ENABLE_URL_BAR_HIDING) &&
+                option.getBoolean(KEY_ENABLE_URL_BAR_HIDING)) {
             builder.enableUrlBarHiding();
         }
-        if (options.hasKey(KEY_SHOW_PAGE_TITLE)) {
-            builder.setShowTitle(options.getBoolean(KEY_SHOW_PAGE_TITLE));
+        if (option.hasKey(KEY_SHOW_PAGE_TITLE)) {
+            builder.setShowTitle(option.getBoolean(KEY_SHOW_PAGE_TITLE));
         }
-        if (options.hasKey(KEY_DEFAULT_SHARE_MENU_ITEM) ||
-                options.getBoolean(KEY_DEFAULT_SHARE_MENU_ITEM)) {
+        if (option.hasKey(KEY_DEFAULT_SHARE_MENU_ITEM) &&
+                option.getBoolean(KEY_DEFAULT_SHARE_MENU_ITEM)) {
             builder.addDefaultShareMenuItem();
         }
-        if (options.hasKey(KEY_ANIMATIONS)) {
+        if (option.hasKey(KEY_ANIMATIONS)) {
             final Context context = getReactApplicationContext();
-            final int animation = options.getInt(KEY_ANIMATIONS);
+            final int animation = option.getInt(KEY_ANIMATIONS);
             switch (animation) {
                 case ANIMATIONS_SLIDE:
                     builder.setStartAnimations(context, R.anim.slide_in_right, R.anim.slide_out_left)
@@ -122,16 +190,6 @@ public class CustomTabsModule extends ReactContextBaseJavaModule {
                     break;
             }
         }
-
-        try {
-            if (getCurrentActivity() != null) {
-                builder.build().launchUrl(getCurrentActivity(), uri);
-                promise.resolve(true);
-            }
-            // TODO: activity == null
-        } catch (Exception e) {
-            promise.reject(new JSApplicationIllegalArgumentException(
-                    "Could not open URL '" + url + "': " + e.getMessage()));
-        }
+        return builder.build();
     }
 }
