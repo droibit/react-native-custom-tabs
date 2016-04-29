4cfe499c4e89cc728bae195059013c24020e036e
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-29 16:50:31 +0900

    Export TabOption type

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ import ChromeIOS from './src/ChromeIOS';
 import CustomTabsAndroid from './src/CustomTabsAndroid';
 import {ANIMATIONS_SLIDE, ANIMATIONS_FADE} from './src/TabOption';
 
+import type {TabOption as _TabOption} from './src/TabOption';
+
+export type TabOption = _TabOption;
+
 const CustomTabs = Platform.OS === 'android' ? CustomTabsAndroid : ChromeIOS;
 export {CustomTabs, ANIMATIONS_SLIDE, ANIMATIONS_FADE};
