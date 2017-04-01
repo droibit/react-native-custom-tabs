55d3134e8f932784b5e356e7682b6dbbb9d8600a
Shinya Kumgai <roomful.rooms@gmail.com>
2017-04-02 06:27:47 +0900

    Change example’s package name

---
+++ example/android/app/src/main/java/com/github/droibit/android/reactnative/customtabs/example/MainApplication.java
@@ -0,0 +1,43 @@
+package com.github.droibit.android.reactnative.customtabs.example;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost reactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.asList(
+                    new MainReactPackage(),
+                    new CustomTabsPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return reactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /*nativeExopackage=*/false);
+    }
+}
